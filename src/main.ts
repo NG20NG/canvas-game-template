2a857ce06023ff7916ca3775bae7dba85fc37c67
unknown <ngnassim20@gmail.com>
2024-06-25 23:26:34 +0100

    first commit

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@
 //
 //
 //
-const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("canvas")! as HTMLCanvasElement;
+
+const ctx = canvas.getContext("2d")! as CanvasRenderingContext2D;
 //
 //
 //
